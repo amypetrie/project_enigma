03c0074c5fe9a46d7fc2e92b342b83f03f62f620
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-08-26 13:17:09 -0600

    WIP - building out encryption logic

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,9 +7,20 @@ class Enigma
     @character_map = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ",", "."]
   end
 
-
   def encrypt(my_message, key = "12345", date = Date.today)
+    ma = my_message.chars.each_slice(4).to_a
+    binding.pry
+    ma.map do |array|
+        array[0] = @character_map[final_rotation_a]
+        array[1] = @character_map[final_rotation_b]
+        array[2] = @character_map[final_rotation_c]
+        array[3] = @character_map[final_rotation_d]
+    end
+  end
 
+  def final_rotation_a
+    final_a = key(rotation_a) + offset(rotation_a)
+    final = final_a % 39
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,4 +17,10 @@ class EnigmaTest < Minitest::Test
     assert_equal ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ",", "."], e.character_map
 
   end
+
+  def test_encryption_works
+    e = Enigma.new
+    message = "hello world"
+    assert_equal "", e.encrypt(message, "12345", Date.today)
+  end
 end
