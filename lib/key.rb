e74791afb33a6a961c41e966d8110afe8561a73f
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-26 11:53:33 -0600

    working on key class and rotations

--- lib/key.rb
+++ lib/key.rb
@@ -2,4 +2,25 @@ require 'pry'
 require 'date'
 
 class Key
+    attr_reader :key_numbers
+  def initialize(key_numbers)
+  @key_numbers = key_numbers
+  end
+
+  def rotation_a
+  end
+
+  def rotation_b
+  end
+
+  def rotation_c
+  end
+
+  def rotation_d
+  end
+
+  def to_array
+  @key_numbers = key_numbers.chars
+  end
+
 end
