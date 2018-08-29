050fadc3d7fd40559ee9ec9b8e01ae4efb8c9864
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-29 07:56:33 -0600

    clean up decrypt runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,16 +1,7 @@
-#runner file, takes four command line arguments:
-#1. encrypted.txt -> contains encrypted msg
-#2. decrypted.txt -> write decrypted msg here
-#3. 5 digit key for decryption (integer)
-#4. 6 digit date (integer)
-# output => < Created 'decrypted.txt' with the key 82648 and date 240818 >
-
 require './lib/enigma'
 require './lib/key'
 require './lib/offset'
 require 'date'
-
-# ARGV == ["message.txt", "encrypted.txt"]
 
 reader_file = File.open(ARGV[0], "r")
 incoming_text = reader_file.read
@@ -18,7 +9,6 @@ reader_file.close
 
 e = Enigma.new
 k = ARGV[2]
-d = Offset.new(ARGV[3])
 
 decrypted_text = e.decrypt(incoming_text.strip, k)
 
